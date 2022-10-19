fcae3cb03921fa1e70c43ea76fde211223ea323f
Tapio Mattila <tapiomattila193@gmail.com>
2022-10-19 22:55:37 +0300

    added helpers and config

---
+++ src/js/helpers.js
@@ -0,0 +1,24 @@
+import { TIMEOUT_SECONDS } from "./config";
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+
+export const getJSON = async function (url) {
+    try {
+        const fetchPromise = fetch(url);
+        const res = await Promise.race([fetchPromise, timeout(TIMEOUT_SECONDS)]);
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
