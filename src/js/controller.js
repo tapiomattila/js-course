a8f7e1023d6d69dc48c12bddc39af9d70815aa09
Tapio Mattila <tapiomattila193@gmail.com>
2022-10-20 19:51:25 +0300

    added recipe servings update

--- src/js/controller.js
+++ src/js/controller.js
@@ -50,7 +50,7 @@ const controlSearchResults = async function () {
   }
 }
 
-const controlPagination = function(goToPage) {
+const controlPagination = function (goToPage) {
   // 1) Render new results
   resultsView.render(model.getSearchResultsPage(goToPage));
 
@@ -58,9 +58,19 @@ const controlPagination = function(goToPage) {
   paginationView.render(model.state.search);
 }
 
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -52,10 +52,16 @@ export const loadSearchResults = async function (query) {
     }
 }
 
-export const getSearchResultsPage = function(page = state.search.page) {
+export const getSearchResultsPage = function (page = state.search.page) {
     state.search.page = page;
     const start = (page - 1) * state.search.resultsPerPage;
     const end = page * state.search.resultsPerPage;
     return state.search.results.slice(start, end);
 }
- 
+
+export const updateServings = function (newServings) {
+    state.recipe.ingredients.forEach(ing => {
+        ing.quantity = ing.quantity * newServings / state.recipe.servings;
+    });
+    state.recipe.servings = newServings;
+}
