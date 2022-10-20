ededef96c05d292217690dac9324066a428c1763
Tapio Mattila <tapiomattila193@gmail.com>
2022-10-20 20:23:20 +0300

    added DOM update algo

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,18 +1,19 @@
 import View from './View';
 
 class ResultsView extends View {
-    _parentElement = document.querySelector('.results');
-    _errorMessage = 'No recipes found for your query! Please try again ;)';
-    _message = '';
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again ;)';
+  _message = '';
 
-    _generateMarkup() {
-        return this._data.map(this._generateMarkupPreview).join('');
-    }
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
 
-    _generateMarkupPreview(result) {
-        return `
+  _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
+    return `
         <li class="preview">
-            <a class="preview__link" href="#${result.id}">
+            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="${result.title}" />
               </figure>
@@ -23,7 +24,7 @@ class ResultsView extends View {
             </a>
           </li>
         `;
-    }
+  }
 }
 
 export default new ResultsView();
