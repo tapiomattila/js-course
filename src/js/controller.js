0f8e6744ded39fa3b8587f6979ffe4b0e0a958dc
Tapio Mattila <tapiomattila193@gmail.com>
2022-10-21 19:51:12 +0300

    added bookmarks localstorage persist

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,15 +22,19 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search results
     resultsView.update(model.getSearchResultsPage());
+
+    // 1) Update bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1) Loading recipe
+    // 2) Loading recipe
     await model.loadRecipe(id);
 
-    // 2) Rendering recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
+
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 }
 
@@ -82,13 +86,17 @@ const controlAddBookmark = function () {
 
   // Update recipe view
   recipeView.update(model.state.recipe);
-  console.log('recipe:', model.state.recipe);
 
   // Render bookmarks
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+}
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
