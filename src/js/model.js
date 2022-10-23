7414216920cdc0bacbe5d03485b6fa1ba161285f
Tapio Mattila <tapiomattila193@gmail.com>
2022-10-23 15:56:57 +0300

    added own recipes and preview icon

--- src/js/model.js
+++ src/js/model.js
@@ -24,6 +24,7 @@ const createRecipeObject = function (data) {
         cookingTime: recipe.cooking_time,
         ingredients: recipe.ingredients,
 
+        /* !! */
         // logic AND returns { key: recipe.key }, when key exists.
         // Then spread the values in the current object)
         ...(recipe.key && { key: recipe.key })
@@ -32,7 +33,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
     try {
-        const data = await AJAX(`${API_URL}${id}`)
+        const data = await AJAX(`${API_URL}${id}?key=${KEY}`)
         state.recipe = createRecipeObject(data);
 
         const isIdFound = state.bookmarks.some(bookmark => bookmark.id === id);
@@ -46,7 +47,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
     try {
         state.search.query = query;
-        const data = await AJAX(`${API_URL}?search=${query}`);
+        const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
 
         state.search.results = data.data.recipes.map(rec => {
             return {
@@ -54,6 +55,7 @@ export const loadSearchResults = async function (query) {
                 title: rec.title,
                 publisher: rec.publisher,
                 image: rec.image_url,
+                ...(rec.key && { key: rec.key })
             }
         });
         state.search.page = 1;
@@ -105,7 +107,8 @@ export const uploadRecipe = async function (newRecipe) {
         const ingredients = Object.entries(newRecipe)
             .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
             .map(ing => {
-                const ingArr = ing[1].replaceAll(' ', '').split(',');
+                // const ingArr = ing[1].replaceAll(' ', '').split(',');
+                const ingArr = ing[1].split(',').map(el => el.trim());
                 if (ingArr.length !== 3) throw new Error('Wrong ingredient format! Please use the correct format :)');
                 const [quantity, unit, description] = ingArr;
                 return { quantity: quantity ? +quantity : null, unit, description };
--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,11 +1,12 @@
 import View from './View';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class PreviewView extends View {
-    _parentElement = '';
+  _parentElement = '';
 
-    _generateMarkup() {
-        const id = window.location.hash.slice(1);
-        return `
+  _generateMarkup() {
+    const id = window.location.hash.slice(1);
+    return `
         <li class="preview">
             <a class="preview__link ${this._data.id === id ? 'preview__link--active' : ''}" href="#${this._data.id}">
               <figure class="preview__fig">
@@ -14,11 +15,17 @@ class PreviewView extends View {
               <div class="preview__data">
                 <h4 class="preview__title">${this._data.title}</h4>
                 <p class="preview__publisher">${this._data.publisher}</p>
+                <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
+                  <svg>
+                    <use href="${icons}#icon-user"></use>
+                  </svg>
+                </div>
               </div>
+             
             </a>
           </li>
         `;
-    }
+  }
 }
 
 export default new PreviewView();
